4ab66e975b279092f0dc28d323d98686adfb8a36
guangLess <bha.guang@gmail.com>
2015-12-02 10:27:47 -0500

    swift

--- swift-summary-function/AppDelegate.swift
+++ swift-summary-function/AppDelegate.swift
@@ -11,14 +11,21 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
         
-        /**
-
-        *  Call your function here!
-
-        */
+        var iosImmersive = "Mobile Development with iOS"
+        var leadInstrotor = "Tim Clem"
+        var studentNum = 20
+        var language = "objective C"
         
-        // Do not alter
-        return true  //
-    }   ///////////////
-}       ///////////////
-
+        summaryForCourse(iosImmersive, leadInstructor: leadInstrotor, language: language, students: studentNum)
+        print (summaryForCourse(iosImmersive, leadInstructor: leadInstrotor, language: language, students: studentNum))
+        
+        iosImmersive = "Mobile Development Corps"
+        leadInstrotor = "Tim Clem"
+        language = "swift"
+        
+        print(summaryForCourse(iosImmersive, leadInstructor: leadInstrotor, language: language, students: studentNum))
+        print(summaryForCourse("Web Development Fellowship class", leadInstructor: "Blake Johnson", language: "Ruby", students: 30))
+    
+        return true
+    }   
+}
--- swift-summary-function/Sandbox.swift
+++ swift-summary-function/Sandbox.swift
@@ -6,4 +6,40 @@ import Foundation
 
 *  Write your function here!
 
+ 
+ func nameOfFunctionArgument1(argument1: Type1, argument2: Type2) -> ReturnType {
+ // implementation
+ }
+ 
+ let capture = nameOfFunctionArgument1(<value>, argument2: <value>)
+
+ }
+ 
+ let iosImmersive = "Mobile Development with iOS"
+ let iosFellowship = "Mobile Development Corps"
+ let webFellowship = "Web Development Fellowship"
+ 
+ let tim = "Tim Clem"
+ let blake = "Blake Johnson"
+ 
+ let swift = "Swift"
+ let ruby = "Ruby"
+ In the Mobile Development with iOS class at the Flatiron School there are 20 students learning Swift from Tim Clem.
+ 
+
 */
+
+
+
+func summaryForCourse (course:String, leadInstructor:String, language: String, students:Int ) -> String{
+    
+    let returnS :String = "In the \(course) class at the Flatiron School there are \(students) students learning \(language) from \(leadInstructor)."
+    
+    //print(returnS)
+    return returnS
+}
+
+
+
+
+
